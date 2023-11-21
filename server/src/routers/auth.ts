c2854acadd85077168da6b24a60b9e62512d4176
oleosb <oleosbo@gmail.com>
2023-11-21 20:09:08 -0300

    Types For Req Object

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -1,9 +1,10 @@
+import { CreateUser } from "#/@types/user";
 import { Router } from "express";
 import User from "models/user";
 
 const router = Router();
 
-router.post("/create", async (req, res) => {
+router.post("/create", async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
   // const user = new User({ email, password, name });
   // user.save();
