c76a51a1dd0e792b55ea4bfce2617c64f1310fb1
oleosb <oleosbo@gmail.com>
2023-11-21 21:35:05 -0300

    Validation Schema

--- server/src/utils/validationSchema.ts
+++ server/src/utils/validationSchema.ts
@@ -11,12 +11,10 @@ export const CreateUserScheama = yup.object().shape({
   password: yup
     .string()
     .trim()
-    .required("Name is missing")
+    .required("Password is missing")
     .min(8, "Password is too short!")
     .matches(
-      /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/,
-      "Pasword is too simple!"
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/,
+      "Password is too simple!"
     ),
 });
-
-CreateUserScheama.validate()
