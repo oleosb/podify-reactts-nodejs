ca0c478d13dc2ac9fc6003d1ce6f2fc15de6f388
oleosb <oleosbo@gmail.com>
2024-01-07 21:04:26 -0300

    Audio Update

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,18 +3,31 @@ import "dotenv/config";
 import "./db";
 
 import authRouter from "./routers/auth";
+import audioRouter from "./routers/audio";
 
 const app = express();
 
-//register out middleware
+// register our middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("src/public"));
 
 app.use("/auth", authRouter);
+app.use("/audio", audioRouter);
 
 const PORT = process.env.PORT || 8989;
 
 app.listen(PORT, () => {
   console.log("Port is listening on port " + PORT);
 });
+
+/**
+ * The plan and features
+ * upload audio files
+ * listen to single audio
+ * add to favorites
+ * create playlist
+ * remove playlist (public-private)
+ * remove audios
+ * many more
+ * */
