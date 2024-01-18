46047ae6d0e93f90137a46b4398611f5c251165a
oleosb <oleosbo@gmail.com>
2024-01-18 17:17:59 -0300

    Get All Histories

--- server/src/routers/history.ts
+++ server/src/routers/history.ts
@@ -1,4 +1,8 @@
-import { removeHistory, updateHistory } from "#/controllers/history";
+import {
+  getHistories,
+  removeHistory,
+  updateHistory,
+} from "#/controllers/history";
 import { mustAuth } from "#/middleware/auth";
 import { validate } from "#/middleware/validator";
 import { UpdateHistorySchema } from "#/utils/validationSchema";
@@ -8,5 +12,6 @@ const router = Router();
 
 router.post("/", mustAuth, validate(UpdateHistorySchema), updateHistory);
 router.delete("/", mustAuth, removeHistory);
+router.get("/", mustAuth, getHistories);
 
 export default router;
