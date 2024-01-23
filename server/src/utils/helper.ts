86cac47e3f62785ef75f7352e874c42794acdfb5
oleosb <oleosbo@gmail.com>
2024-01-23 16:59:11 -0300

    Get Auto Generated Playlist

--- server/src/utils/helper.ts
+++ server/src/utils/helper.ts
@@ -1,4 +1,7 @@
+import History from "#/models/history";
 import { UserDocument } from "#/models/user";
+import { Request } from "express";
+import moment from "moment";
 
 export const generateToken = (length = 6) => {
   // declare a variable
@@ -22,4 +25,38 @@ export const formatProfile = (user: UserDocument) => {
     followers: user.followers.length,
     followings: user.followings.length,
   };
+};
+
+export const getUsersPreviousHistory = async (
+  req: Request
+): Promise<string[]> => {
+  const [result] = await History.aggregate([
+    { $match: { owner: req.user.id } },
+    { $unwind: "$all" },
+    {
+      $match: {
+        "all.date": {
+          // only those histories which are not older than 30 days
+          $gte: moment().subtract(30, "days").toDate(),
+        },
+      },
+    },
+    { $group: { _id: "$all.audio" } },
+    {
+      $lookup: {
+        from: "audios",
+        localField: "_id",
+        foreignField: "_id",
+        as: "audioData",
+      },
+    },
+    { $unwind: "$audioData" },
+    { $group: { _id: null, category: { $addToSet: "$audioData.category" } } },
+  ]);
+
+  if (result) {
+    return result.category;
+  }
+
+  return [];
 };
